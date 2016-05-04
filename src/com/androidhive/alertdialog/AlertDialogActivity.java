36d36c3211c3bae451e150ba6afd79cdc58244fa
Reetika Ahuja <reetika_ahuja@rvtechnologies.co.in>
2016-05-04 10:12:52 +0530

    Alert Dialogs

--- src/com/androidhive/alertdialog/AlertDialogActivity.java
+++ src/com/androidhive/alertdialog/AlertDialogActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class AlertDialogActivity extends Activity {
-	/** Called when the activity is first created. */
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
